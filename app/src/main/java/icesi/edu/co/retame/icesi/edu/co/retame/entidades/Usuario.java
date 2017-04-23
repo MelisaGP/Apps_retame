744388f91d30d3f12390cf62ac30dd315b949b28
juan <jdcmena@outlook.com>
2017-04-23 12:30:21 -0500

    clase usuario

---
+++ app/src/main/java/icesi/edu/co/retame/icesi/edu/co/retame/entidades/Usuario.java
@@ -0,0 +1,109 @@
+package icesi.edu.co.retame.icesi.edu.co.retame.entidades;
+
+/**
+ * Created by JuanDavid on 23/4/2017.
+ */
+
+public class Usuario {
+
+    public static final char ALUMNO = 'A';
+    public static final char TUTOR = 'T';
+
+    private String nombre;
+    private String contraseña;
+    private String usuario;
+    private char tipoUsuario;
+    private int puntaje;
+
+    public Usuario(String nombre, String usuario, String contraseña, char tipoUsuario, int puntaje) {
+        this.nombre = nombre;
+        this.usuario = usuario;
+        this.contraseña = contraseña;
+        this.tipoUsuario = tipoUsuario;
+        this.puntaje = puntaje;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Crea un grupo, se generará el codigo al momento de crear
+     * @param nombreGrupo nombre del grupo
+     * @param descripcion descripcion del grupo
+     */
+    public void crearGrupo(String nombreGrupo, String descripcion){
+        //TODO
+    }
+
+    /**
+     * Elimina un grupo dado un codigo
+     * @param grupoId codigo de grupo
+     */
+    public void eliminarGrupo(int grupoId){
+        //TODO
+    }
+
+    /**
+     * Obtiene un grupo dado un código
+    // * @param grupoId codigo del grupo
+     * @return objeto Grupo
+     */
+    /*
+    public Grupo buscarGrupo(int grupoId){
+        return null;
+    }
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /////GETS & SETS
+
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public char getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(char tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
+
+    public int getPuntaje() {
+        return puntaje;
+    }
+
+    public void cambiarPuntaje(int puntaje) {
+        this.puntaje = puntaje;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void cambiarContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+}
