b592667a5fe1765ff7213aab41c7d1d82a88e780
JD <jdcmena@outlook.com>
2017-05-05 00:06:44 -0500

    Se creó la clase Reto, con sus atributos y la declaración de los metodos

---
+++ app/src/main/java/icesi/edu/co/retame/entidades/Reto.java
@@ -0,0 +1,41 @@
+package icesi.edu.co.retame.entidades;
+
+/**
+ * Created by jdcm on 4/05/17.
+ */
+
+public class Reto {
+
+
+    private String nombre;
+    private String descripcionReto;
+    private int numeroParticipantes;
+    private char nivelDificultad;
+
+    public final static char FACIL= 'F';
+    public final static char MEDIO= 'M';
+    public final static char DIFICIL= 'D';
+    public final static char MUY_DIFICIL= 'I';
+
+    public Reto(String nombre, String descripcionReto, char nivelDificultad) {
+
+    }
+
+    public boolean agregarUsuario(Usuario user){
+
+        return false;
+    }
+
+    public void cambiarNombre(String nombre){
+
+        this.nombre=nombre;
+
+    }
+
+    public void cambiarDescripcio(String descripcionReto){
+
+        this.descripcionReto=descripcionReto;
+
+    }
+
+}
